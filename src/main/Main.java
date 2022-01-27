e85e7eccafd16cbfe69710c0c332d316c4ed44cd
Anna Vasilyeva <shevchenko2447@gmail.com>
2022-01-27 15:14:05 +0300

    add second test

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello java");
+        System.out.println("test");
+        System.out.println("create test branch");
     }
 }
