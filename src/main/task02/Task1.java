082a7ba7f3e53cf7c32ed8c71398ce9eb6497f97
anyavasilyeva <shevchenko2447@gmail.com>
2023-04-20 11:25:39 +0300

    commit

--- src/main/task02/Task1.java
+++ src/main/task02/Task1.java
@@ -5,7 +5,7 @@ public class Task1 {
         for(int i = 2; i <= 100; i ++){
             boolean isPrime = true;
 
-            for(int j = 2; j < i; j++){
+            for(int j = 3; j < i; j++){
                 if(i % j == 0){
                     isPrime = false;
                     break;
@@ -20,3 +20,4 @@ public class Task1 {
     }
 }
 //простые числа - это те числа, которые имеют только Два делителя
+// просто числа
