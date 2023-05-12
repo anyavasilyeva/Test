3da654af917e805fc844247ca2b6828708cc368b
anyavasilyeva <shevchenko2447@gmail.com>
2023-05-12 21:04:19 +0300

    test

---
+++ src/main/task04/Task1.java
@@ -0,0 +1,14 @@
+package main.task04;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String name = "Вася";
+
+        if(name.equals("Вася")) {
+            System.out.println("Привет");
+            System.out.println("Как дела?");
+        }
+
+
+    }
+}
